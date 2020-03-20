c4538f7102808b72bbcbfcfac03826d51cfa7bd3
tayxar <62136369+tayxar@users.noreply.github.com>
2020-03-20 13:20:29 +0630

    Update server.js

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ var app = require('express')(); // Create an instance of an Express app
 
 var mobileApp = require('azure-mobile-apps')(); // Create an instance of a Mobile App with default settings
 
-mobileApp.tables.add('CheckIn'); // Create a table for 'Book' with default settings
-mobileApp.tables.add('Users'); 
+mobileApp.tables.add('CheckIn'); // Create a table for 'CheckIn' with default settings
+mobileApp.tables.add('Users'); //Create a table 
 
 app.use(mobileApp);
 app.listen(process.env.PORT || 3000);
