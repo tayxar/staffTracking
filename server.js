d9401db3010b779d54beb95cad76e0024c1b66db
tayxar <62136369+tayxar@users.noreply.github.com>
2020-03-31 15:55:38 +0630

    Change Table Name

--- server.js
+++ server.js
@@ -2,11 +2,11 @@ var app = require('express')(); // Create an instance of an Express app
 
 var mobileApp = require('azure-mobile-apps')(); // Create an instance of a Mobile App with default settings
 
-mobileApp.tables.add('CheckIn'); // Create a table for 'CheckIn' with default settings
-mobileApp.tables.add('Users'); //Create a table 
+mobileApp.tables.add('E_CheckIn'); // Create a table for 'CheckIn' with default settings
+mobileApp.tables.add('E_Users'); //Create a table 
 mobileApp.tables.add('E_Employee'); //Create Table E_Employee
-mobileApp.tables.add('EmployeeSite'); //EmployeeSite
-mobileApp.tables.add('Site');
+mobileApp.tables.add('E_EmployeeSite'); //EmployeeSite
+mobileApp.tables.add('E_Site');
 
 app.use(mobileApp);
 app.listen(process.env.PORT || 3000);
